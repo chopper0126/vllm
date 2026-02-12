de5335674e201565dc795482eba851f0bba41022
yangxiaojun0126 <yangxiaojun41@huawei.com>
2026-02-12 17:17:25 +0800

    update_mla_attn_params for DBO,fix FIA

--- vllm/distributed/kv_transfer/kv_connector/v1/shared_storage_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/shared_storage_connector.py
@@ -259,6 +259,7 @@ class SharedStorageConnector(KVConnectorBase_V1):
             the number of tokens that can be loaded from the 
             external KV cache beyond what is already computed.
         """
+        return len(request.prompt_token_ids) - 1, False
         # NOTE: in this debug implementation, we assume that the prompt is
         # cached_prompt + newly_generated_single_token
         # Therefore, we use prompt_token_ids[:-1] to determine the folder name
